c4dfe1d73f2f94bc28d04f28a5239547136014b3
mbenoukaiss <marwanebenoukaiss@aol.com>
2020-04-06 16:02:12 +0200

    Template and CSS files

--- src/DependencyInjection/FilterSearchExtension.php
+++ src/DependencyInjection/FilterSearchExtension.php
@@ -12,8 +12,21 @@ class FilterSearchExtension extends Extension
 
     public function load(array $configs, ContainerBuilder $container)
     {
-        $loader = new XmlFileLoader($container, new FileLocator(__DIR__."/../Resources/config"));
+        $loader = new XmlFileLoader($container, new FileLocator(__DIR__ . "/../../config"));
         $loader->load("services.xml");
+
+        $this->loadTwigTheme($container);
     }
 
+    private function loadTwigTheme(ContainerBuilder $container)
+    {
+        if(!$container->hasParameter('twig.form.resources')) {
+            return;
+        }
+
+        $container->setParameter('twig.form.resources', array_merge([
+            '@FilterSearch/filter_search.html.twig',
+            '@FilterSearch/search_modules.html.twig',
+        ], $container->getParameter('twig.form.resources')));
+    }
 }
